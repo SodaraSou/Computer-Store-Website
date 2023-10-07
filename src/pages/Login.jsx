865ae1481c877537b332dfc09cb02895ba6f30ee
Sak Khakna <sthissakkhakna@gmail.com>
2023-10-07 13:38:38 +0700

    login first commit

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,28 @@
+import LoginImage from '../assets/LoginImage.jpg'
 function Login() {
-    return (<h1>Hello World</h1>)
+    return (
+        <div className="w-full min-h-screen flex items-start">
+            <div className="w-1/2 h-full flex flex-col">
+                <img src={LoginImage} className="w-full h-full"/>
+            </div>
+
+            <div className="w-1/2 h-full bg-[#D9D9D9] flex flex-col p-20">
+                <div className="w-full flex flex-col">
+                    <h1 className="text-4xl font-bold mb-9">Login</h1>
+                </div>
+                <div className="w-full flex flex-col">
+                    <h2 className="text-xl font-bold mb-4">Email</h2>
+                    <input type="email" className="w-[390px] h-[50px] rounded-md mb-4"/>
+                    <h2 className="text-xl font-bold mb-4">Password</h2>
+                    <input type="password" className="w-[390px] h-[50px] rounded-md mb-4"/>
+                    <div className="flex justify-between">
+                        <p></p>
+                        <p className="text-xl font-bold mb-4 cursor-pointer">Forgot Password ?</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Login;
