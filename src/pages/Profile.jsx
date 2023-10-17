3d2497829c097be534687e912bdac73a1720c10e
Sodara Sou <sodarasou168@gmail.com>
2023-10-17 21:38:17 +0700

    Finished new layout

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -53,7 +53,7 @@ function Profile() {
               <h1 className="font-bold text-2xl md:text-4xl">My Profile</h1>
               <button
                 onClick={logOut}
-                className="bg-red-500 px-3 py-2 rounded-lg"
+                className="bg-red-500 h-10 w-32 rounded-full font-bold"
               >
                 Log Out
               </button>
@@ -160,19 +160,35 @@ function Profile() {
           </div>
           {/* Order History */}
           <div className="w-full border rounded-xl p-4 md:p-10">
-            <h1 className="text-2xl md:text-4xl font-bold mb-4">
-              Order History
-            </h1>
-            <div className="flex justify-between">
-              <div className="w-full">
-                <h1 className="font-bold">Date of Order 01/12/2023 11:12 AM</h1>
-                <p>Order number: 000000001</p>
-                <p>Order status: Complete</p>
-                <p>Delivery by: J&T</p>
+            <h1 className="text-2xl md:text-4xl font-bold">Order History</h1>
+            <div className="h-[1px] w-full bg-[#D9D9D9] mt-4 mb-10"></div>
+            <div className="flex flex-col gap-4 md:gap-10">
+              <div className="flex justify-between">
+                <div className="w-full">
+                  <h1 className="font-bold">
+                    Date of Order 01/12/2023 11:12 AM
+                  </h1>
+                  <p>Order number: 000000001</p>
+                  <p>Order status: Complete</p>
+                  <p>Delivery by: J&T</p>
+                </div>
+                <button className="h-10 w-32 rounded-full bg-[#D9D9D9] font-bold">
+                  Detail
+                </button>
               </div>
-              <button className="h-10 w-32 rounded-full bg-[#D9D9D9] font-bold">
-                Detail
-              </button>
+              <div className="flex justify-between">
+                <div className="w-full">
+                  <h1 className="font-bold">
+                    Date of Order 01/12/2023 11:12 AM
+                  </h1>
+                  <p>Order number: 000000001</p>
+                  <p>Order status: Complete</p>
+                  <p>Delivery by: J&T</p>
+                </div>
+                <button className="h-10 w-32 rounded-full bg-[#D9D9D9] font-bold">
+                  Detail
+                </button>
+              </div>
             </div>
           </div>
         </div>
