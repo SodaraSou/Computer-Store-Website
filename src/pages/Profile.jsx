b9687532af68601bb5fb88075e99849ce5b47156
Sodara Sou <sodarasou168@gmail.com>
2023-10-17 21:57:29 +0700

    Added edit button to profile

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -59,11 +59,17 @@ function Profile() {
               </button>
             </div>
             <div className="flex flex-col items-center md:flex-row gap-10">
-              <img
-                src="https://static.vecteezy.com/system/resources/previews/001/840/612/non_2x/picture-profile-icon-male-icon-human-or-people-sign-and-symbol-free-vector.jpg"
-                alt="Profile Image"
-                className="rounded-full w-40 h-40"
-              />
+              <div className="w-full md:w-1/4 flex flex-col gap-4 justify-center items-center">
+                <img
+                  src="https://static.vecteezy.com/system/resources/previews/001/840/612/non_2x/picture-profile-icon-male-icon-human-or-people-sign-and-symbol-free-vector.jpg"
+                  alt="Profile Image"
+                  className="rounded-full w-40 h-40"
+                />
+                <button className="text-lg flex items-center gap-2">
+                  <img src={EditSvg} alt="edit" width={18} height={18} />
+                  Edit Profile
+                </button>
+              </div>
               <div className="w-full flex flex-col gap-4">
                 <div className="flex gap-2">
                   <div className="w-full flex flex-col gap-2">
